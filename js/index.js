c2264b18099cc59bfafc4373e43f11623be1310d
NissimBet <nissimbet123@outlook.com>
2020-01-18 15:50:46 -0600

    finalizado lab 4

--- js/index.js
+++ js/index.js
@@ -1,7 +1,46 @@
-import "jquery";
+//import 'jquery';
 
-$(document).ready(() => {
+const ShoppingItem = ({ name }) => `
+  <li>
+    <p>${name}</p>
+    <div class="button-container">
+      <button class="check-button">Check</button>
+      <button class="delete-button">Delete</button>
+    </div>
+  </li>
+`;
 
+function init() {
+  const shoppingForm = $('#shoppingCartForm');
+  const checklist = $('#checklist');
+  const itemInput = $('#itemInput');
+
+  $(shoppingForm).on('submit', event => {
+    event.preventDefault();
+
+    if ($(itemInput).val() !== "") {
+      $(checklist).append(ShoppingItem({
+        name: $(itemInput).val()
+      }));
+      
+      var items = $(checklist).children('li').get();
+      items.sort((a, b) => $(a).children('p').text().toLowerCase().localeCompare($(b).children('p').text().toLowerCase()));
   
+      $(checklist).empty()
+      $(checklist).append(items);
+    
+  
+      $(itemInput).val('');
+    }
+  });
 
-});
+  $(checklist).on('click', '.check-button', event => {
+    $(event.target).closest('li').children('p').toggleClass('crossed');
+  })
+
+  $(checklist).on('click', '.delete-button', event => {
+    $(event.target).closest('li').remove();
+  })
+}
+
+init();
